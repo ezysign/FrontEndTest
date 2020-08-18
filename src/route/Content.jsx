839d7862492989c2156a4aed19024f298606bcc5
Htin Wana <htinwana@gmail.com>
2020-08-18 16:24:49 +0630

    Format File.

--- src/route/Content.jsx
+++ src/route/Content.jsx
@@ -1,25 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
-import Home from "../pages/HomePage/Home";
-import Login from "../pages/LoginPage/Login";
-import ProtectedRoute from "./ProtectedRoute";
+import React, { useEffect, useState } from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import Home from '../pages/HomePage/Home';
+import Login from '../pages/LoginPage/Login';
+import ProtectedRoute from './ProtectedRoute';
 
 function Content() {
   const [logged, setLogged] = useState(false);
 
   useEffect(() => {
-    if (localStorage.getItem('token')){
-      setLogged(true)
-    }else {
-      setLogged(false)
+    if (localStorage.getItem('token')) {
+      setLogged(true);
+    } else {
+      setLogged(false);
     }
-  },[])
+  }, []);
 
   return (
     <BrowserRouter>
       <Switch>
-        <ProtectedRoute exact path="/home" component={Home} logged={logged} setLogged={setLogged} />
-        <Route exact path="/login" render={props => (<Login {...props} logged={logged} setLogged={setLogged} />)} />
+        <ProtectedRoute exact path='/home' component={Home} logged={logged} setLogged={setLogged} />
+        <Route
+          exact
+          path='/login'
+          render={(props) => <Login {...props} logged={logged} setLogged={setLogged} />}
+        />
       </Switch>
     </BrowserRouter>
   );
