e4d3d00db0e8163f4e912dafce81d17a95bd1ec2
Htin Wana <htinwana@gmail.com>
2020-08-18 16:23:15 +0630

    Format file.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Content from "./route/Content";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Content from './route/Content';
 
 function App() {
   return <Content />;
