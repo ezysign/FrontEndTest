e24db4dcd7fa85fe0aaf73e98e18e00810b62601
ted <heinhtetnaing186@gmail.com>
2020-08-20 00:39:01 +0630

    second

--- src/test.js
+++ src/test.js
@@ -1,88 +1,19 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Button from "@material-ui/core";
+
 import { increment, decrement } from "./home";
-import { login } from "./login";
 
-describe("Increment list", () => {
-  assert({
-    given: "a click action and the current number",
-    should: "add 1 to the number divide by 6",
-    actual: increment(3, click()),
-    expected: 4,
-  });
+it("should increment the num value by 1 until it gets to 6 and it reset to 0", () => {
+  const tree = shallow(<Button handleClick={increment} />);
+
+  tree.simulate("click");
+  expect(5).toEqual(0);
 });
 
-describe("Increment list", () => {
-  assert({
-    given: "a click action and the current number",
-    should: "add 1 to the number divide by 6",
-    actual: increment(5, click()),
-    expected: 0,
-  });
-});
+it("should decrement the num value by 1 until it gets to 0 and it reset to 6", () => {
+  const tree = shallow(<Button handleClick={decrement} />);
 
-describe("Decrement list", () => {
-  assert({
-    given: "a click action and the current number",
-    should: "minus 1 to the number and it becomes 5 if its < 0",
-    actual: increment(3, click()),
-    expected: 2,
-  });
-});
-
-describe("Decrement list", () => {
-  assert({
-    given: "a click action and the current number",
-    should: "minus 1 to the number and it becomes 5 if its < 0",
-    actual: increment(0, click()),
-    expected: 5,
-  });
-});
-
-describe("login", () => {
-  assert({
-    given: "a click action and email / password ",
-    should:
-      "check with the api if the email and password matches. return 400 if it doesnt, 200 if it does",
-    actual: login({ email: "", passowrd: "" }, click()),
-    expected: 400,
-  });
-});
-
-describe("login", () => {
-  assert({
-    given: "a click action and email / password ",
-    should:
-      "check with the api if the email and password matches. return 400 if it doesnt, 200 if it does",
-    actual: login({ email: "asdfasdf", passowrd: "1234" }, click()),
-    expected: 400,
-  });
-});
-
-describe("login", () => {
-  assert({
-    given: "a click action and email / password ",
-    should:
-      "check with the api if the email and password matches. return 400 if it doesnt, 200 if it does",
-    actual: login({ email: "asdfasdf", passowrd: "" }, click()),
-    expected: 400,
-  });
-});
-
-describe("login", () => {
-  assert({
-    given: "a click action and email / password ",
-    should:
-      "check with the api if the email and password matches. return 400 if it doesnt, 200 if it does",
-    actual: login({ email: "", passowrd: "1234" }, click()),
-    expected: 400,
-  });
-});
-
-describe("login", () => {
-  assert({
-    given: "a click action and email / password ",
-    should:
-      "check with the api if the email and password matches. return 400 if it doesnt, 200 if it does",
-    actual: login({ email: "blahblah@gmail.com", passowrd: "1234" }, click()),
-    expected: 200,
-  });
+  tree.simulate("click");
+  expect(2).toEqual(1);
 });
