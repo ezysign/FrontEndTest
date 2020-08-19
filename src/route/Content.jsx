736b28778357abd2f82bf997dbcc2558cac5ff33
Htin Wana <htinwana@gmail.com>
2020-08-19 10:42:59 +0630

    Fix routes.
    "/" does not exist so nothing is rendered there.

--- src/route/Content.jsx
+++ src/route/Content.jsx
@@ -1,29 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter, Switch, useHistory } from 'react-router-dom';
 import Home from '../pages/HomePage/Home';
 import Login from '../pages/LoginPage/Login';
 import ProtectedRoute from './ProtectedRoute';
 
 function Content() {
   const [logged, setLogged] = useState(false);
+  const token = localStorage.getItem('token');
 
   useEffect(() => {
-    if (localStorage.getItem('token')) {
+    if (token) {
       setLogged(true);
     } else {
       setLogged(false);
     }
-  }, []);
+  }, [token]);
 
   return (
     <BrowserRouter>
       <Switch>
-        <ProtectedRoute exact path='/home' component={Home} logged={logged} setLogged={setLogged} />
         <Route
           exact
+          path='/'
+          render={(props) => <Login {...props} logged={logged} setLogged={setLogged} />}
+        />
+        <Route
           path='/login'
           render={(props) => <Login {...props} logged={logged} setLogged={setLogged} />}
         />
+        <ProtectedRoute exact path='/home' component={Home} logged={logged} setLogged={setLogged} />
       </Switch>
     </BrowserRouter>
   );
